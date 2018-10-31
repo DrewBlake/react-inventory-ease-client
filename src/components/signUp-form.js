948803ca554da5c511312ed45a8bd5b88695fe09
Andrew Blake <drewblake1974@gmail.com>
2018-10-31 18:12:00 -0400

    started components

---
+++ src/components/signUp-form.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
+
+
+export class SignupForm extends React.Component {
+    render() {
+        return (
+            <form >
+                <label for="first-name">First name</label>
+                <input placeholder='First Name' type="text" 
+                       name='first-name' id='first-name' 
+                       ref={input => (this.input = input)}
+                       required />     
+                <button >
+                    Sign Up
+                </button>
+            </form>
+        );
+    }
+}
+
+export default connect()(SignupForm);
